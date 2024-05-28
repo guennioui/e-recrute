3e2be787e9651efb66437eaa1b44920ded93c729
guennioui <guenniouiabdellah2002@gmail.com>
2024-05-28 23:14:29 +0100

    correction d'une erreur - RecruiterTypeRepository

--- src/main/java/ma/emsi/erecrute/repositories/RecruiterTypeRepository.java
+++ src/main/java/ma/emsi/erecrute/repositories/RecruiterTypeRepository.java
@@ -1,6 +1,7 @@
 package ma.emsi.erecrute.repositories;
 
+import ma.emsi.erecrute.entites.RecruiterType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecruiterTypeRepository extends JpaRepository<RecruiterTypeRepository, Long> {
+public interface RecruiterTypeRepository extends JpaRepository<RecruiterType, Long> {
 }
